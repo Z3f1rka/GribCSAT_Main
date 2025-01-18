4e1df7883ebd3d901adfb25e7aa10a974daa43df
Alina <AlinaK73@yandex.ru>
2025-01-18 18:27:12 +0400

    added table with products without orm

--- alembic/env.py
+++ alembic/env.py
@@ -12,6 +12,7 @@ from sqlalchemy import pool
 from app.core.config import settings
 from app.db.database import Base
 from app.db.models.user import User
+from app.db.models.product import Product
 
 sys.path.append(os.path.join(sys.path[0], 'app'))
 
---
+++ app/db/models/product.py
@@ -0,0 +1,16 @@
+from sqlalchemy import BigInteger, String
+from sqlalchemy.orm import Mapped, mapped_column
+
+from app.db.database import Base
+
+
+class Product(Base):
+    __tablename__ = "products"
+
+    id: Mapped[int] = mapped_column(BigInteger, primary_key=True, index=True)
+    link: Mapped[str] = mapped_column(String, nullable=False)
+    title: Mapped[str] = mapped_column(String, nullable=False)
+    # type orm to type.id
+    article: Mapped[str] = mapped_column(String, nullable=False)
+    file: Mapped[str]
+    # shop orm to shop.id
