6bd24cc1119d667f508e3bed3cf3509addd0494e
Z3f1rka <z.e.f.i.r.k.a88877@gmail.com>
2025-01-25 19:42:33 +0400

    Add Questions

--- app/db/all_models.py
+++ app/db/all_models.py
@@ -7,3 +7,4 @@ from app.db.models.visit import Visit
 from app.db.models.types import Types
 from app.db.models.surveys import Surveys
 from app.db.models.product_list import ProductList
+from app.db.models.questions import Questions
